9248a7ef41170a9f6c61b0082e07f15a2d3692a2
João Júnior <122739421+joao-junior-dev@users.noreply.github.com>
2025-09-30 13:14:13 -0300

    Project initial

---
+++ main.py
@@ -0,0 +1,5 @@
+# Project: Github User Activity:
+# Tasks:
+# Criar uma classe que representará o usuário do github
+# Criar uma função que irá buscar os eventos do usuario
+# Criar um CLI para lidar com pedidos 
